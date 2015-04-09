2adabb21e7fbe4488f5e871e1a6db81b8d0f1dd2
Angela Castanieto <angela@Jeffs-Mac.(none)>
2015-04-08 22:41:48 -0700

    Moved type report generation into function

--- accounting.py
+++ accounting.py
@@ -1,18 +1,24 @@
-SALESPERSON_INDEX = 0
-INTERNET_INDEX = 1
-DORKY_LINE_LENGTH = 80
+# This line not needed?  SALESPERSON_INDEX = 0
+# This line not needed? INTERNET_INDEX = 1
+section_separator = 80
 
-print "*" * DORKY_LINE_LENGTH
-f = open("orders-by-type.txt")
+print "*" * section_separator
+
 melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
 
-for l in f:
-    data = l.split("|")
-    melon_type = data[1]
-    melon_count = int(data[2])
-    melon_tallies[melon_type] += melon_count
+def gen_orders_by_type_report(file_name):    #"orders-by-type.txt"
+    open_file = open(file_name)
 
-f.close()
+
+    for line in open_file:
+        data = line.split("|")
+        melon_type = data[1]
+        melon_count = int(data[2])
+        melon_tallies[melon_type] += melon_count
+
+    f.close()
+
+
 melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
 total_revenue = 0
 for melon_type in melon_tallies:
@@ -20,7 +26,9 @@ for melon_type in melon_tallies:
     revenue = price * melon_tallies[melon_type]
     total_revenue += revenue
     print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-print "******************************************"
+
+print "*" * section_separator
+
 f = open("orders-with-sales.txt")
 sales = [0, 0]
 for line in f:
@@ -35,4 +43,6 @@ if sales[1] > sales[0]:
     print "Guess there's some value to those salespeople after all."
 else:
     print "Time to fire the sales team! Online sales rule all!"
-print "******************************************"
+print "*" * section_separator
+
+gen_orders_by_type_report("orders-by-type.txt")
